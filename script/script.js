f8f5817f1c93f13d576f3480b2553e3d80a0058f
enrique <enrique.corpus.5004@hotmail.com>
2025-07-18 12:39:35 -0500

    first class

---
+++ script/script.js
@@ -0,0 +1,43 @@
+//######################
+//##### Dia1 ###########
+//######################
+
+// Primer comando
+console.log("Hola Mundo");
+
+// Number
+variable1= 3.14;
+console.log(variable1);
+
+//concatenacion
+
+palabra1= "enrique";
+palabra2= 55000;
+palabra3= palabra1 + palabra2;
+console.log(palabra3);
+
+
+//condicionales
+boleanito = true;
+
+if (boleanito == true){
+    console.log("es verdadero");
+} 
+else{ console.log("no es verdadero");
+}
+
+console.log(boleanito);
+//ciclo 
+
+
+//Bucles
+for(i=0;i<4;i=i+1){
+    console.log("Wenas");
+}
+
+holi = true;
+while(holi==true){
+     console.log("Wenass");
+     holi=false;
+}
+//Desarrolado por Enrique Corpus cc 1143405301
