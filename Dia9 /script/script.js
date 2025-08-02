f24539aa9748a58a29061f92ffdae0690a626685
Enrique <enrique.corpus.5004@hotmail.com>
2025-08-01 19:34:36 -0500

    feat :fire: add buttoms next and prev

--- Dia9 /script/script.js
+++ Dia9 /script/script.js
@@ -1,6 +1,10 @@
-function buscarPersonaje(){
+let idPokemon = 0
 
-    const nombreUsar = document.getElementById("searchPokemon").value.trim().toLowerCase();
+function buscarPersonaje(idPokemon1){
+
+    const inputValor = document.getElementById("searchPokemon").value.trim().toLowerCase();
+    const nombreUsar = idPokemon1 || inputValor || "1";
+
     const xhr = new XMLHttpRequest();
     const url = `https://pokeapi.co/api/v2/pokemon/${nombreUsar}`;
     console.log(url);
@@ -14,11 +18,13 @@ function buscarPersonaje(){
 
                 const daticos = JSON.parse(xhr.responseText);
 
-                let imgPokemon = daticos.sprites.front_default
+                let imgPokemon = daticos.sprites.other.showdown.front_default
 
                 document.getElementById("imgPokemon").src = imgPokemon
 
                 document.getElementById("idNombre").textContent =  daticos.id + " - " + daticos.name
+
+                idPokemon= daticos.id;
             }
 
             catch(err){
@@ -32,4 +38,25 @@ xhr.send();
 
  }
 
- document.getElementById("searchPokemon").addEventListener("input", buscarPersonaje);
+ document.getElementById("searchPokemon").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        buscarPersonaje();
+    }
+});
+
+document.getElementById("next").addEventListener("click", function () {
+    idPokemon++;
+    buscarPersonaje(idPokemon);
+    document.getElementById("searchPokemon").value = "";
+});
+
+
+document.getElementById("prev").addEventListener("click", function () {
+    if (idPokemon > 1) {
+        idPokemon--;
+        buscarPersonaje(idPokemon);
+        document.getElementById("searchPokemon").value = "";
+    }
+});
+
+
