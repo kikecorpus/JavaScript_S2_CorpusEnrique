9021991349902ed72cef22e07ffe70c3fb464f07
Enrique <enrique.corpus.5004@hotmail.com>
2025-08-02 21:12:12 -0500

    feat: :fire: implement basic hand evaluation (par, three, poke, and double par)

--- Dia10 /poker.js
+++ Dia10 /poker.js
@@ -148,20 +148,60 @@ const convertirCartas = {
 "5": 5, "4": 4, "3": 3, "2": 2
 };
 
+//extraer valor nuevos  y palos
 let nuevosValores = mano.map(carta => convertirCartas[carta.value]);
 let categoria = mano.map(palo => palo.suit);
 
 console.log(nuevosValores)
 console.log(categoria)
 
-diccionarioPares = {}
-par = nuevosValores.forEach( valor => {diccionarioPares[valor] = (diccionarioPares[valor] || 0) + 1;
+// contar pares o trios, o poker 
+let diccionarioPares = {}
+
+nuevosValores.forEach( valor => {diccionarioPares[valor] = (diccionarioPares[valor] || 0) + 1;
+
+console.log(diccionarioPares)
   
-});
+}); 
 
+//verificar si existe 1 par 
+let pares = [];
+let trio = [];
+let poker = []
 
+for (let valor in diccionarioPares) {
 
+  if (diccionarioPares[valor] === 2) {
+    pares.push(parseInt(valor)); 
+  }
+  else if (diccionarioPares[valor] === 3) {
+    trio.push(parseInt(valor)); 
+  }
+
+  else if (diccionarioPares[valor] === 4) {
+  poker.push(parseInt(valor)); 
+  }
 }
+  // verificar si tiene poker
+  if (poker.length === 1){
+  console.log("Tienes poker")
+  }
+  // verificar si existe trio
+  else if (trio.length === 1){
+    console.log("Tienes trio")
+  }
+  //verificar si tiene par 
+  else if (pares.length === 2) {
+  console.log("Tienes doble par")
+  }
+ //verificar si tiene dobrepar 
+  else if ((pares.length === 1) && (pares[0] > 10)) {
+  console.log("Tienes un par de j o mas");
+  } 
+ }
+
+
+// algoritmo
 armarMazo()
 
 document.getElementById("pedir").onclick = pedir;
